85aa5d43191d418a4ba3fdf5cac567d95b16849c
Andrej Noskaj <andrej.nosky@gmail.com>
2022-09-14 18:27:21 +0200

    improved scrapper.

--- src/utils/scrapper.py
+++ src/utils/scrapper.py
@@ -8,47 +8,49 @@ df = pd.read_csv("data/scrapped.csv", index_col="id")
 article_data = {
     "slovenskeslovo": {
         "total": 2670,
-        "parsed": 1898
+        "parsed": 1898,
+        "domain": "slovenskeslovo.sk"
     },
     "ta3": {
         "total": 245922,
-        "parsed": 2526
+        "parsed": 2788,
+        "domain": "ta3.com/clanok"
     }
 }
 
 # Selector to get correct quantities.
-selector = "slovenskeslovo"
+selector = "ta3"
 
 for article_decrement in range(article_data[selector]['total'] - article_data[selector]['parsed']):
-    print(f"Articles parsed: {article_decrement + article_data[selector]['parsed']}")
-    req = requests.get(f"https://slovenskeslovo.sk/{article_data[selector]['total'] - article_decrement}")
+    print(f"Articles parsed: {article_decrement + article_data[selector]['parsed']} / {article_data[selector]['total']}")
+    req = requests.get(f"https://{article_data[selector]['domain']}/{article_data[selector]['total'] - article_decrement}")
 
     # Parsing is too specific for each platform, so it is solved using comments.
     soup = BeautifulSoup(req.content, 'html5lib')
 
     try:
         # TA3
-        # title = soup.find('h1', attrs={'class': 'article-title'}).get_text().strip().replace("\xa0", " ")W
+        title = soup.find('h1', attrs={'class': 'article-title'}).get_text().strip().replace("\xa0", " ")
         # slovenskeslovo
-        title = soup.find('h2', attrs={'class': 'avatar-article-heading'}).get_text().strip().replace("\xa0", " ")
+        # title = soup.find('h2', attrs={'class': 'avatar-article-heading'}).get_text().strip().replace("\xa0", " ")
     except Exception:
         title = ""
 
     try:
         # TA3
-        # perex = soup.find('div', attrs={'class': 'article-perex'}).get_text()
+        perex = soup.find('div', attrs={'class': 'article-perex'}).get_text()
         # slovenskeslovo
-        perex = ""
+        # perex = ""
     except Exception:
         perex = ""
 
     try:
         # TA3
-        # content = soup.find('div', attrs={'class': 'article-component'}).find_all('p')
-        # content = list(map(lambda paragraph: paragraph.get_text().strip().replace("\xa0", " "), content))
-        # slovenskeslovo
-        content = soup.find('div', attrs={'class': 'item-page'}).find_all('p')
+        content = soup.find('div', attrs={'class': 'article-component'}).find_all('p')
         content = list(map(lambda paragraph: paragraph.get_text().strip().replace("\xa0", " "), content))
+        # slovenskeslovo
+        # content = soup.find('div', attrs={'class': 'item-page'}).find_all('p')
+        # content = list(map(lambda paragraph: paragraph.get_text().strip().replace("\xa0", " "), content))
 
         article = perex.join(content)
     except Exception:
